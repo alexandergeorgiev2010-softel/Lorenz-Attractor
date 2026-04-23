3083e18f7b08a7eea3dfb5fbfb449a98840abac3
alexandergeorgiev2010-softel <alexandergeorgiev2010@gmail.com>
2026-04-23 22:01:10 +0300

    Update AnimationsLorenzo.py

--- AnimationsLorenzo.py
+++ AnimationsLorenzo.py
@@ -15,7 +15,7 @@ def PointsForGoal(function, state0, time, dt = 0.01):
 
 class LorenzAttractor(ThreeDScene):
     def construct(self):
-        # 1. УРАВНЕНИЯ С ГРАДИЕНТ
+      
         equations = MathTex(
             r"\dot{x} = \sigma(y - x)\\",
             r"\dot{y} = x(\rho - z) - y\\",
@@ -23,11 +23,10 @@ class LorenzAttractor(ThreeDScene):
             font_size=40
         ).to_corner(UL).set_stroke(width=1)
         
-        # Прилагане на градиент Синьо -> Розово
+        
         equations.set_color_by_gradient("#00FFFF", "#FF00FF")
         self.add_fixed_in_frame_mobjects(equations)
 
-        # 2. НАСТРОЙКА НА КАМЕРА И ОСИ
         self.set_camera_orientation(phi = 75 * DEGREES, theta = - 45 * DEGREES, zoom = 0.8)
         axes = ThreeDAxes(
             x_range = (-50, 50, 5), y_range = (-50, 50, 5), z_range = (0, 50, 5),
@@ -35,7 +34,7 @@ class LorenzAttractor(ThreeDScene):
         )
         self.add(axes)
 
-        # 3. ТРАЕКТОРИИ И ЦВЕТОВЕ
+        
         epsilon = 0.001
         states = [[10, 10, 10], [10, 10, 10 + epsilon]] 
         colors = ["#00FFFF", "#FF00FF"] 
@@ -49,13 +48,13 @@ class LorenzAttractor(ThreeDScene):
             curve.set_color(colors[i]) 
             curves.add(curve)
         
-        # 4. ТОЧКИ (АНИМАТОРИ)
+        
         dots = Group(*[Dot3D(color=color, radius = 0.05).scale(1.5) for color in colors])
         
         for dot, curve in zip(dots, curves):
             dot.move_to(curve.get_start())
 
-        # Update функция за движението на точките
+        
         def update_dots(dots):
             for dot, curve in zip(dots, curves):
                 # Тъй като Create анимацията рисува кривата, 
@@ -64,7 +63,6 @@ class LorenzAttractor(ThreeDScene):
         
         dots.add_updater(update_dots)
 
-        # 5. СТАРТ НА АНИМАЦИЯТА
         self.begin_ambient_camera_rotation(rate = 0.05)
         self.add(dots)
         self.play(*[Create(curve) for curve in curves], run_time = 15, rate_func=linear)
